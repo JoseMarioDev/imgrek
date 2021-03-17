f7684e2c3b2870c5a1f0a44ffbda3097e0c61773
JoseMarioDev <josemariodev@gmail.com>
2021-03-17 06:28:38 -0500

    :art: adding lambda Fn and code

--- app/lib/app-stack.ts
+++ app/lib/app-stack.ts
@@ -18,6 +18,45 @@ export class AppStack extends cdk.Stack {
     const imageBucket = new s3.Bucket(this, imageBucketName, {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
-    new cdk.CfnOutput(this, 'imageBucket', { value: imageBucket.bucketName });
+    new cdk.CfnOutput(this, 'imageBucket', { value: imageBucket.bucketName }); // cloudformation output
+
+    // =====================================================================================
+    // Amazon DynamoDB table for storing image labels
+    // =====================================================================================
+    const table = new dynamodb.Table(this, 'ImageLabels', {
+      partitionKey: { name: 'image', type: dynamodb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+    new cdk.CfnOutput(this, 'ddbTable', { value: table.tableName });
+
+    // =====================================================================================
+    // Building our AWS Lambda Function; compute for our serverless microservice
+    // =====================================================================================
+    const rekFn = new lambda.Function(this, 'rekognitionFunction', {
+      code: lambda.Code.fromAsset('rekognitionlambda'),
+      runtime: lambda.Runtime.PYTHON_3_7,
+      handler: 'index.handler',
+      timeout: Duration.seconds(30),
+      memorySize: 1024,
+      environment: {
+        TABLE: table.tableName,
+        BUCKET: imageBucket.bucketName,
+      },
+    });
+    rekFn.addEventSource(
+      new event_sources.S3EventSource(imageBucket, {
+        events: [s3.EventType.OBJECT_CREATED],
+      }),
+    );
+    imageBucket.grantRead(rekFn);
+    table.grantWriteData(rekFn);
+
+    rekFn.addToRolePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ['rekognition:DetectLabels'],
+        resources: ['*'],
+      }),
+    );
   }
 }
