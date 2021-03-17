51eb803f26bde538e7b856769c51a59f202be3c6
JoseMarioDev <josemariodev@gmail.com>
2021-03-17 05:58:08 -0500

    :heavy_plus_sign: adding deps from aws

--- app/lib/app-stack.ts
+++ app/lib/app-stack.ts
@@ -1,9 +1,23 @@
 import * as cdk from '@aws-cdk/core';
+import s3 = require('@aws-cdk/aws-s3');
+import iam = require('@aws-cdk/aws-iam');
+import dynamodb = require('@aws-cdk/aws-dynamodb');
+import lambda = require('@aws-cdk/aws-lambda');
+import event_sources = require('@aws-cdk/aws-lambda-event-sources');
+import { Duration } from '@aws-cdk/core';
+
+const imageBucketName = 'imgrek-imagebucket';
 
 export class AppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // =====================================================================================
+    // Image Bucket
+    // =====================================================================================
+    const imageBucket = new s3.Bucket(this, imageBucketName, {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+    new cdk.CfnOutput(this, 'imageBucket', { value: imageBucket.bucketName });
   }
 }
